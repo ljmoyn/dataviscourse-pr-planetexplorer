47c2dc2002ed5f14bc367facd3cf69109ae112f5
ljmoyn <ljmoyn@gmail.com>
2019-10-31 14:51:37 -0600

    data cleanup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-d3.csv("data/confirmed-planets.csv").then(dataCSV => {
+d3.csv("data/confirmed-planets.csv").then(rawData => {
 
-    debugger;
+    data  = rawData.map(function(d){
+        return {
+            id: Number(d.loc_rowid),
+            mass: Number(d.pl_bmassj),
+            discoveryMethod: d.pl_discmethod,
+            facility: d.pl_facility,
+            name: d.pl_name,
+            radius: Number(d.pl_radj),
+            lastUpdate: d.rowupdate,
+            distance: Number(d.st_dist)
+        }
+    })
 });
